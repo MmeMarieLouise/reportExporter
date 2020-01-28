5774838a56f7029af94d9e309c69afc59116f032
Marie-Louise Annan <ml_annan@hotmail.com>
2020-01-28 13:04:35 +0000

    import flask_bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import create_engine
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
-
+bootstrap = Bootstrap(app)
 
 
 from app import routes
