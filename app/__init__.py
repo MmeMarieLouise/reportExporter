b98b6300c9217ad618863302c336f3b183a37014
Marie-Louise Annan <ml_annan@hotmail.com>
2020-01-28 13:35:31 +0000

    update

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from sqlalchemy import create_engine
 from flask_bootstrap import Bootstrap
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -14,6 +15,7 @@ bootstrap = Bootstrap(app)
 
 from app import routes
 
+# create engine and connect before the request is made
 @app.before_request
 def create_postgres_conn():
     engine = create_engine('postgresql+psycopg2://interview:uo4uu3AeF3@candidate.suade.org/suade')
