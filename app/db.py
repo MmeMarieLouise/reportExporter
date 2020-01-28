30672f05fd016f7bd37994d9e9edb0a2f4f4f7fd
Marie-Louise Annan <ml_annan@hotmail.com>
2020-01-28 13:12:58 +0000

    update

--- app/db.py
+++ app/db.py
@@ -3,19 +3,19 @@ from sqlalchemy import create_engine
 # create engine object to connect to remote postgres server
 engine = create_engine('postgresql+psycopg2://interview:uo4uu3AeF3@candidate.suade.org/suade')
 
-# connect to remote server and returns a connect object, inside () can execute sql commands to query database, if it returns a proxy object - then use fetchall()
-#  o = conn.execute('select * from reports')
-#  o.fetchall()
-
+# connect to remote server
 conn = engine.connect()
 
+# query tables
 all_tables = engine.table_names()
 
+# inside () can execute sql commands to query database, if it returns a proxy object - then use fetchall()
 results = conn.execute('select * from reports')
 
 for row in results:
     print(row)
 
+# close connection
 conn.close()
 
 
